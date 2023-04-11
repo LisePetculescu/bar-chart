c12baee8048cc1361e41b289db87b37802a0c8d7
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-04-11 21:18:16 +0200

    gg

--- script.js
+++ script.js
@@ -2,14 +2,19 @@
 console.log("script.js is scripting...");
 window.addEventListener("load", start);
 
-const numberList = [
-  23, 25, 19, 3, 5, 15, 28, 19, 28, 29, 4, 1, 27, 24, 5, 13, 3, 18, 24, 10, 24,
-  30, 19, 16, 13, 23, 27, 27, 9, 23, 6, 20, 9, 4, 23, 2, 0, 17, 30, 6,
-];
+const numberList = [];
 
 function start() {
   showBars();
+  customers();
   setInterval(getNumberOfCustomers, 1000);
+}
+
+function customers() {
+  for (let i = 0; i < 40; i++) {
+    let number = Math.floor(Math.random() * 32);
+    numberList.push(number);
+  }
 }
 
 function getNumberOfCustomers() {
